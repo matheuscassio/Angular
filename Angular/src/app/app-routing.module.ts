ba2fef0319205619c6a516c1cd6a34c2450f592e
Matheus Cassio <mtcassio13@gmail.com>
2022-08-05 20:53:16 -0300

    atualizações...

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { HomeComponent } from './components/home/home.component';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
   {
     path: '',component: NavComponent, children:[
-      {path: 'home', component: HomeComponent }
+      {path: 'home', component: HomeComponent },
+      {path: 'tecnicos', component: TecnicoListComponent}
     ]
   }
 
